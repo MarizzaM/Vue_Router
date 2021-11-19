e6bfefdca33245e1c9cd939a1ab827fbca90f4ae
Pavlova Marina <marizza.mil89@gmail.com>
2021-11-19 23:42:51 +0200

    06_RedirectsAnd404's

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
-
-import JobDetails from '../views//jobs/JobDetails.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -21,15 +21,23 @@ const routes = [
     name: 'Jobs',
     component: Jobs
   },
-
   {
     path: '/jobs/:id',
     name: 'JobDetails',
     component: JobDetails,
-
     props: true
   },
-
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // 404 catchall
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
