5cf960549833e0766184f11a8714978266f95248
Pavlova Marina <marizza.mil89@gmail.com>
2021-11-20 12:04:36 +0200

    09_FetchingDataFromComponents

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,17 +1,22 @@
 <template>
-  <h1>Job Details</h1>
-  <!-- <p>The job id is {{ $route.params.id }}</p> -->
+  <h1>{{ job.title }}</h1> <!-- gets an error -->
   <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-//   data() {
-//     return {
-//       id: this.$route.params.id
-//     }
-//   }
+  props: ['id'],
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
